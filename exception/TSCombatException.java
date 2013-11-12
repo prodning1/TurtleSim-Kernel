e49c14e2dd7c0f44a88c18900a321628d3351c0d
Philip Rodning <philip.rodning@gmail.com>
2013-11-12 10:23:05 -0500

    initial commit

---
+++ exception/TSCombatException.java
@@ -0,0 +1,10 @@
+package com.prodning.turtlesim.kernel.exception;
+
+/**
+ * Created by phider on 11/12/13.
+ */
+public class TSCombatException {
+    public TSCombatException(String message) {
+        super(message);
+    }
+}
