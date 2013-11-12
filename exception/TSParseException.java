b372c3f2dc45bc1d1daab28ac6512e15b1482fc6
Philip Rodning <philip.rodning@gmail.com>
2013-11-12 10:27:27 -0500

    initial commit

---
+++ exception/TSParseException.java
@@ -0,0 +1,10 @@
+package com.prodning.turtlesim.kernel.exception;
+
+/**
+ * Created by phider on 11/12/13.
+ */
+public class TSParseException extends Exception {
+    public TSParseException(String message) {
+        super(message);
+    }
+}
